455b8abfe1b1552c1e8634f4139ee30f391777f5
Sharanagouda Konasirasagi <sharanagouda_k@etvmm119786.local>
2019-03-01 17:00:54 +0530

    second update

--- src/component/Main.js
+++ src/component/Main.js
@@ -11,7 +11,8 @@ class Main extends Component {
             usersData: [
                 { id: 1, name: "Sharanagouda", contactno: "9876543210", pincode:516408 },
                 { id: 2, name: "Devid", contactno: "1236547890",pincode:560012 },
-                { id: 3, name: "john", contactno: "1245789630", pincode:585103 }
+                { id: 3, name: "john", contactno: "1245789630", pincode:585103 },
+                { id: 4, name: "Kumar", contactno: "9874662540", pincode:661087 }
               ],
         }
     }
